cad320fc87b1793f26f9f8ffbe72e0aa64713a09
meek0 <cedricgfont@gmail.com>
2018-11-09 14:47:02 -0500

    sample R code

--- src/main/conf/templates/validate/routine.R
+++ src/main/conf/templates/validate/routine.R
@@ -1,19 +1,9 @@
-library(googlesheets4)
-library(validate)
-library(magrittr)
-library(rlang)
-
-ssId <- "https://docs.google.com/spreadsheets/d/1Hu9hZGa44mnwqnW21A-IDMAXobPiWJMVdzXxC6xLpHg/edit#gid=742454028"
-
-loadTibble <- function(ssId) {
-  ss <- sheets_get(ssId)
-  sheetName <- ss$sheets$name[1]
-  sheets_read(ss$spreadsheet_id, sheetName)
-}
+rulesValues <- payload$expressions
+rules <- unlist(strsplit(rulesValues, split=','))
 
 df <- loadTibble(ssId)
 
-idf <- data.frame(rule=c("sex > 0", "age < 80"), label=c("Sex", "Age"))
-i <- validator(.data=idf)
+idf <- data.frame(rule=rules)
+i <- indicator(.data=idf)
 c <- confront(df, i)
 summary(c)
