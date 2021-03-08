7f8329223fe3e5a91d784d6dd0effd621206a7ef
Yannick Marcon <yannick.marcon@obiba.org>
2021-03-08 23:07:59 +0100

    opal 4.0

--- src/main/java/org/obiba/analysis/opal/validate/ValidateAnalysisService.java
+++ src/main/java/org/obiba/analysis/opal/validate/ValidateAnalysisService.java
@@ -1,8 +1,8 @@
 package org.obiba.analysis.opal.validate;
 
+import org.obiba.opal.spi.r.RServerResult;
 import org.obiba.opal.spi.r.analysis.AbstractRAnalysisService;
 import org.obiba.opal.spi.r.analysis.RAnalysis;
-import org.rosuda.REngine.REXP;
 
 public class ValidateAnalysisService extends AbstractRAnalysisService {
 
@@ -13,7 +13,7 @@ public class ValidateAnalysisService extends AbstractRAnalysisService {
   }
 
   @Override
-  protected REXP run(RAnalysis analysis) {
+  protected RServerResult run(RAnalysis analysis) {
     ValidateAnalysisROperation validateAnalysisROperation = new ValidateAnalysisROperation(analysis);
     analysis.getSession().execute(validateAnalysisROperation);
     return validateAnalysisROperation.getResult();
